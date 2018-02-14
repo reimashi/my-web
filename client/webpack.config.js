1961f3bfc357e39d78d5f177cd586664281cdeaf
Aitor González Fernández <reimashi@gmail.com>
2018-02-14 22:56:02 +0100

    Webpack optimizado para produccion

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,6 +1,20 @@
+const DEBUG = false;
+
+const webpack = require("webpack");
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
 module.exports = {
     entry: "./app/app.js",
     output: {
         filename: './app/app.bundle.js'
-    }
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            '$': "jquery",
+            'jQuery': "jquery",
+            'Popper': 'popper.js'
+        })
+    ].concat(DEBUG ? [] : [
+        new UglifyJsPlugin()
+    ])
 };
