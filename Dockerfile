070c9ee986612e04f93ecfd0cf5c5ccd4dbd1074
Aitor González Fernández <reimashi@gmail.com>
2018-02-21 20:11:30 +0100

    Script de build y docker actualizado para el servidor

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,23 @@
+FROM golang:latest
+
+RUN apt-get update && \
+    apt-get install git
+
+RUN export CGO_ENABLED=0 && \
+    export GOOS=linux
+
+WORKDIR /go/src/app
+COPY ./server .
+
+RUN go get -d -v ./...
+
+RUN mkdir -p /build && \
+    go build -a -installsuffix cgo -o /build/server .
+
+RUN chmod +x /build/server
+
 FROM scratch
+WORKDIR /app
+COPY --from=0 /build/server .
 
-ENV HTTP_PORT 80
-
-ADD build/server /
-
-ENTRYPOINT ["/server"]
+ENTRYPOINT ['/app/server']
--- build.sh
+++ build.sh
@@ -1,28 +1,30 @@
 #!/bin/bash
 
-CLIENTDIR="./client"
-SERVERDIR="./server"
+Version="0.1"
 
-BUILDDIR="./build"
-CLIENTBD="$BUILDDIR/static"
+ClientDir="$(pwd)/client"
+ServerDir="$(pwd)/server"
+
+BuildDir="$(pwd)/dist"
+ClientBuildDir="$BuildDir/static"
 
 function compile_server {
     if ! type go > /dev/null 2>&1; then
         echo "Instalando dependencia: golang"
         sudo add-apt-repository ppa:gophers/archive > /dev/null
         sudo apt-get update > /dev/null
-        sudo apt-get install golang-1.9-go > /dev/null
+        sudo apt-get install golang-1.9-go
     fi
 
     echo "Compilando el servidor..."
-    CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o $BUILDDIR/server $SERVERDIR
+    CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o ${BuildDir}/server ${ServerDir}
 }
 
 function compile_client {
     if ! type node > /dev/null 2>&1; then
         echo "Instalando dependencia: Node.js"
         curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-        sudo apt-get install -y nodejs > /dev/null
+        sudo apt-get install -y nodejs
     fi
 
     if ! type yarn > /dev/null 2>&1; then
@@ -35,13 +37,37 @@ function compile_client {
         sudo npm install -g webpack
     fi
 
-    mkdir -p $CLIENTBD
-    cd $CLIENTDIR
+    mkdir -p ${ClientDir}
+    cd ${ClientDir}
 
     echo "Compilando el código javascript del cliente..."
-    webpack --config webpack.config.build.js
+    webpack --env.BuildDir=${BuildDir} --config webpack.config.build.js
 
     cd ..
+}
+
+function compile_docker {
+    if ! type docker > /dev/null 2>&1; then
+        echo "Instalando dependencia: docker"
+        sudo apt-get install apt-transport-https ca-certificates curl software-properties-common -y 2> /dev/null
+        curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+        sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+        sudo apt-get update > /dev/null
+        sudo apt-get install docker-ce -y
+    fi
+
+    ImageName="reimashi/my-web"
+
+    echo "Compilando la imagen de docker..."
+    docker build . -t ${ImageName}:${Version} -t ${ImageName}:latest
+
+    echo "Enviando imagen a DockerHub..."
+    FirstPushResult=`docker push ${ImageName}:${Version}`
+    if [ ! ${FirstPushResult} ]; then
+        docker login
+        docker push ${ImageName}:${Version}
+    fi
+    docker push ${ImageName}:latest
 }
 
 # Install dependencies
@@ -51,22 +77,22 @@ fi
 
 if ! type dialog > /dev/null 2>&1; then
     echo "Instalando dependencia: dialog"
-    sudo apt-get install dialog -y > /dev/null
+    sudo apt-get install dialog -y
 fi
 
 # Make build directory
-if [ ! -d $BUILDDIR ]; then
-    sudo rm -Rf $BUILDDIR
+if [ ! -d ${BuildDir} ]; then
+    sudo rm -Rf ${BuildDir}
 fi
-mkdir -p $BUILDDIR
+mkdir -p ${BuildDir}
 
-WHATDO=`/usr/bin/dialog --stdout --checklist 'Elije los paquetes a compilar:' 80 40 20 1 Servidor on 2 'Cliente web' on 3 'Repositorio docker' on`
+WhatDo=`/usr/bin/dialog --stdout --checklist 'Elije los paquetes a compilar:' 80 40 20 1 'Servidor' off 2 'Cliente web' on 3 'Repositorio docker' off`
 
 if [ $? -eq 0 ]
 then
-    for TASK in $WHATDO
+    for Task in ${WhatDo}
     do
-        case $TASK in
+        case ${Task} in
         1)
             compile_server
             ;;
